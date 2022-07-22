3d7a3d496a0c703fc79712cd737b633914c3e46c
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-07-22 16:14:55 +0900

    a

---
+++ Mission-MVC2/src/main/java/kr/ac/kopo/filter/EncodingFilter.java
@@ -0,0 +1,52 @@
+package kr.ac.kopo.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter(
+		urlPatterns = { "*" }, 
+		initParams = { 
+				@WebInitParam(name = "encoding", value = "utf-8")
+		})
+public class EncodingFilter implements Filter {
+
+	private FilterConfig config;
+	
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("filter...");
+		
+		request.setCharacterEncoding(config.getInitParameter("encoding"));
+		
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		System.out.println("filter 시작...");
+		this.config = fConfig;
+	}
+
+}
